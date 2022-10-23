4cb9b3fed14de070326aefdf1dd1f94ea61d087f
junggmin <101504594+junggmin@users.noreply.github.com>
2022-10-24 05:00:26 +0900

    Update clock.js

--- js/clock.js
+++ js/clock.js
@@ -2,20 +2,28 @@ const clock = document.querySelector("h2#clock");
 var hours;
 var minutes;
 var seconds;
+var flag;
+
 function mouseDownHandler() {
-  clock.innerText = "저기..지금..몇시야???";
+  flag = 1;
 }
 function mouseOverHandler() {
-  clock.innerText = `${hours}:${minutes}:${seconds}`;
+  flag = 0;
 }
+
 function getClock() {
   const date = new Date();
   hours = String(date.getHours()).padStart(2, "0");
   minutes = String(date.getMinutes()).padStart(2, "0");
   seconds = String(date.getSeconds()).padStart(2, "0");
+  if (flag === 0) {
+    clock.innerText = `${hours}:${minutes}:${seconds}`;
+  } else {
+    clock.innerText = "저기..지금..몇시야???";
+  }
 }
+
 getClock();
 setInterval(getClock, 1000);
-
 clock.addEventListener("mouseleave", mouseDownHandler);
 clock.addEventListener("mouseover", mouseOverHandler);
